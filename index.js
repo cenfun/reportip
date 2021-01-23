53f8464153eb5dc69edce5f8b0ffdcf4577c9439
cenfun <cenfun@gmail.com>
2021-01-23 15:35:24 +0800

    add checkIp

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
-const ip = require("ip");
+const IP = require("ip");
 const DF = require("dateformat");
 const nodemailer = require("nodemailer");
 
-
-const now = new Date();
-
-console.log(DF(now, "yyyy-mm-dd HH:MM:ss"));
-
 const option = {
+    pollingInterval: 5 * 1000,
     transport: {
         host: "",
         port: 25
@@ -20,15 +16,34 @@ const option = {
     }
 };
 
-const sendEmail = async () => {
+const sendEmail = async (html) => {
     const transport = option.transport;
     const transporter = nodemailer.createTransport(transport);
     const emailOption = option.email;
-
-    emailOption.html = ip.address();
-
+    emailOption.html = html;
     console.log("sending email ...");
     const info = await transporter.sendMail(emailOption);
     console.log(info);
 };
 
+let currentIp;
+const checkIp = function() {
+    const ip = IP.address();
+    if (ip === currentIp) {
+        return;
+    }
+    currentIp = ip;
+    const now = new Date();
+    const date = DF(now, "yyyy-mm-dd HH:MM:ss");
+    option.email.subject = `IP:${ip} ${date}`;
+    sendEmail(option.email.subject);
+};
+
+const start = function() {
+    checkIp();
+    setInterval(function() {
+        checkIp();
+    }, option.pollingInterval);
+};
+
+start();
